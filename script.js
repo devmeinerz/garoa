d62747ba6c9a3bca8e2249568e5808a081796faf
Raul Meinerz <devmeinerz@gmail.com>
2021-09-06 21:46:38 -0300

    troca de valores e botoes play/pause

--- script.js
+++ script.js
@@ -3,12 +3,66 @@ var audio02 = document.getElementById("audio02");
 var audio03 = document.getElementById("audio03");
 var audio04 = document.getElementById("audio04");
 
+var botaoRain = document.getElementById("rain");
+var botaoBolt = document.getElementById("bolt");
+var botaoWind = document.getElementById("wind");
+var botaoWave = document.getElementById("wave");
+
+var botaoPlay01 = document.getElementById("rain-play");
+var botaoPlay02 = document.getElementById("bolt-play");
+var botaoPlay03 = document.getElementById("wind-play");
+var botaoPlay04 = document.getElementById("wave-play");
+
+
 // ============== VOLUMES DE INICIALIZAÇÃO ===========
 
 audio01.volume = 0.5;
-audio02.volume = 0.05;
-audio03.volume = 0.05;
-audio04.volume = 0.05;
+audio02.volume = 0.5;
+audio03.volume = 0.5;
+audio04.volume = 0.5;
+
+// ============== BOTÕES PLAY/PAUSE DOS SONS ===========
+
+function playOrPause01(){
+    
+    if (audio01.paused){
+        audio01.play();
+        botaoPlay01.src="/assets/images/pause.png";
+    }else{
+        audio01.pause();
+        botaoPlay01.src="/assets/images/play.png";
+    }
+};
+
+function playOrPause02(){
+    if (audio02.paused){
+        audio02.play();
+        botaoPlay02.src="/assets/images/pause.png";
+    }else{
+        audio02.pause();
+        botaoPlay02.src="/assets/images/play.png";
+    }
+};
+
+function playOrPause03(){
+    if (audio03.paused){
+        audio03.play();
+        botaoPlay03.src="/assets/images/pause.png";
+    }else{
+        audio03.pause();
+        botaoPlay03.src="/assets/images/play.png";
+    }
+};
+
+function playOrPause04(){
+    if (audio04.paused){
+        audio04.play();
+        botaoPlay04.src="/assets/images/pause.png";
+    }else{
+        audio04.pause();
+        botaoPlay04.src="/assets/images/play.png";
+    }
+};
 
 // ============== CONTROLA O VOLUME DOS SLIDERS ===========
 
@@ -28,7 +82,7 @@ function setVolume4(value) {
 // ============== REMOVE O GAP DO LOOP DE AUDIO DO HTML5 ===========
 
 audio01.addEventListener('timeupdate', function () {
-    var buffer = 1
+    var buffer = .99
     if (this.currentTime > this.duration - buffer) {
         this.currentTime = 0
         this.play()
@@ -36,7 +90,7 @@ audio01.addEventListener('timeupdate', function () {
 }, false);
 
 audio02.addEventListener('timeupdate', function () {
-    var buffer = 1
+    var buffer = .99
     if (this.currentTime > this.duration - buffer) {
         this.currentTime = 0
         this.play()
@@ -44,7 +98,7 @@ audio02.addEventListener('timeupdate', function () {
 }, false);
 
 audio03.addEventListener('timeupdate', function () {
-    var buffer = 1
+    var buffer = .99
     if (this.currentTime > this.duration - buffer) {
         this.currentTime = 0
         this.play()
@@ -52,7 +106,7 @@ audio03.addEventListener('timeupdate', function () {
 }, false);
 
 audio04.addEventListener('timeupdate', function () {
-    var buffer = .40
+    var buffer = .99
     if (this.currentTime > this.duration - buffer) {
         this.currentTime = 0
         this.play()
